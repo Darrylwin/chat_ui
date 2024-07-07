69590778854f11f91cc64f67338b0824806949da
Darrylwin <logossoudarryl20@gmail.com>
2024-07-07 18:54:08 +0000

    section 'alla' et specification du chemin vers asset/images/

--- lib/main.dart
+++ lib/main.dart
@@ -32,77 +32,159 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF171717),
-      body: Column(
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 70,
-              left: 5,
-              right: 5,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.menu,
-                    color: Colors.white,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 70,
+                  left: 5,
+                  right: 5,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.only(
+                    left: 10,
                   ),
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Messages",
+                        style: TextStyle(color: Colors.white, fontSize: 26),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Online",
+                        style: TextStyle(color: Colors.grey, fontSize: 26),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Groups",
+                        style: TextStyle(color: Colors.grey, fontSize: 26),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "More",
+                        style: TextStyle(color: Colors.grey, fontSize: 26),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 35,
+                    ),
+                  ],
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.search),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 50,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Messages",
-                    style: TextStyle(color: Colors.white, fontSize: 26),
+          Positioned(
+            top: 190,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.only(
+                top: 15,
+                left: 25,
+                right: 25,
+              ),
+              height: 220,
+              decoration: BoxDecoration(
+                color: Color(0xFF27c1A9),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(40)),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Favorites contacts",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.more_horiz),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Online",
-                    style: TextStyle(color: Colors.grey, fontSize: 26),
+                  SizedBox(
+                    height: 90,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 32,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                radius: 29,
+                                backgroundImage:
+                                    Image.asset("assets/images/img1.jpeg")
+                                        .image,
+                              ),
+                            ),
+                            SizedBox(height: 3),
+                            Text(
+                              "Alla",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Groups",
-                    style: TextStyle(color: Colors.grey, fontSize: 26),
-                  ),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "More",
-                    style: TextStyle(color: Colors.grey, fontSize: 26),
-                  ),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
